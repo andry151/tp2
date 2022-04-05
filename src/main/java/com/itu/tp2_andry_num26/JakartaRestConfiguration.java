98d75fe1d9e2798477c13f5efb287f826379b735
Andry Razafindrazaka <andry.razafind@gmail.com>
2022-04-05 18:56:49 +0200

    "Tp2 Modification"

---
+++ src/main/java/com/itu/tp2_andry_num26/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.itu.tp2_andry_num26;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Andry
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/com/itu/tp2_andry_num26/config/ConfigJSF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itu.tp2_andry_num26.config;
+
+/**
+ *
+ * @author Andry
+ */
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
